7c4a96faafc7e4016cc5de99b95806e1d6c24fd1
isabella-34 <isamolial34@gmail.com>
2026-04-30 00:43:24 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -121,7 +121,7 @@ apps = [
     {"img": "deco.png", "titulo": "Análisis de sentimiento", "url": "https://sentimientooo.streamlit.app/"},
     {"img": "deco.png", "titulo": "Tablerito", "url": "https://tableritooo.streamlit.app/"},
     {"img": "deco.png", "titulo": "TF-IDF Español", "url": "https://tdf--espanol.streamlit.app/"},
-    {"img": "OIG6.jpg", "titulo": "Sistema Físico", "url": "https://vision2-gpt4o.streamlit.app/"},
+    {"img": "deco.png", "titulo": "Texto a voz", "url": "https://textovozcopia.streamlit.app/"},
     {"img": "OIG6.jpg", "titulo": "Sistema Físico", "url": "https://vision2-gpt4o.streamlit.app/"},
     {"img": "OIG6.jpg", "titulo": "Sistema Físico", "url": "https://vision2-gpt4o.streamlit.app/"},
 ]
